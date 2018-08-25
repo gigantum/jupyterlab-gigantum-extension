f120d288c76c5579f022926fd141fa6e35868275
Dav Clark <dav@gigantum.com>
2018-08-25 13:19:56 -0400

    Towards a complete initial widget

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import GigantumWidget from './gigantum';
+
 import '../style/index.css';
 
 
@@ -12,6 +14,8 @@ const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab-gigantum-extension',
   autoStart: true,
   activate: (app: JupyterLab) => {
+    const gigantumWidget = new GigantumWidget();
+    app.shell.addToLeftArea(gigantumWidget, {rank: 102});
     console.log('JupyterLab extension jupyterlab-gigantum-extension is activated!');
   }
 };
