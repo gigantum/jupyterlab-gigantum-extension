c21e6117819c4ebbb276cae9ca0a71d4b9cceb1b
Dean Kleissas <dean@gigantum.com>
2018-08-25 13:39:36 -0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+//import { INotebookTracker } from '@jupyterlab/notebook';
+
+import {
+  Widget, PanelLayout
+} from '@phosphor/widgets';
+
 import '../style/index.css';
 
 
@@ -11,8 +21,38 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab-gigantum-extension',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab-gigantum-extension is activated!');
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+    console.log('Updated');
+    console.log('ICommandPalette:', palette);
+    let widget: Widget = new Widget();
+    widget.id = 'gigantum-jupyterlab';
+    //let notebookTracker: INotebookTracker = notebook_Tracker;
+
+    widget.title.iconClass = 'jp-Gigantum-icon jp-SideBar-tabIcon';
+    widget.title.caption = 'Gigantum';
+    widget.addClass("jp-Gigantum")
+
+    let header = document.createElement('h1');
+    header.textContent = "TEST"
+    widget.node.appendChild(header);
+
+    let dropdown = document.createElement('select');
+    dropdown.id = "mySelect";
+    var trackingOptions = ["Auto","Always Show","Always Hide","Ignore"];
+    for (var i = 0; i < trackingOptions.length; i++) {
+        var option = document.createElement("option");
+        option.value = trackingOptions[i];
+        option.text = trackingOptions[i];
+        dropdown.appendChild(option);
+    }
+    widget.node.appendChild(dropdown);
+
+    let layout = new PanelLayout();
+    layout.addWidget(widget);
+
+    app.shell.addToLeftArea(widget, { rank: 10 });
+
   }
 };
 
